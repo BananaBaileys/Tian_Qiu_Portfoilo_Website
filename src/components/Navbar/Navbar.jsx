d072c548e9c7b957471b91cf00ee9be2861720a5
BananaBaileys <tian987654321@gmail.com>
2023-04-10 21:56:19 -0400

    updated header
    worked on some header, having an idea the looks of the header for all pages. also a uniform style for the whole site in index.css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import images from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => (
-    <div>
-        Navbar
-    </div>
-);
+    <nav className="app__navbar">
+      <div className="app__navbar-pp">
+        <img src={images.profilepic} alt="profile pic" />
+      </div>
+      <ul className="app__navbar-links"> 
+        <li className="p__opensans"> <a href="#home">Home</a> </li>
+        <li className="p__opensans"> <a href="#about">About Me</a> </li>
+        <li className="p__opensans"> <a href="#menu">Projects</a> </li>
+        <li className="p__opensans"> <a href="#awards">Photos</a> </li>
+        <li className="p__opensans"> <a href="#contact">Contact</a> </li>
+      </ul>
+     
+     </nav>
+); 
 
 export default Navbar;
---
+++ src/constants/images.js
@@ -0,0 +1,5 @@
+import profilepic from '../assets/400x400 profile pic linkedin.jpg';
+
+export default {
+    profilepic,
+};
